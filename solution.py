a98e32a4f3c6673da87b95a3792d0bbd3912ec60
flakepowders <42159553+flakepowders@users.noreply.github.com>
2022-11-18 00:42:39 +0900

    Update solution.py
    각 Block이 모두 squareWidth, squareHeight을 따로 저장할 필요가 없으므로 전역변수로 분리했습니다.
    merge 함수를 도로 지웠습니다. 다른 더 좋은 구조가 있을까요?

--- solution.py
+++ solution.py
@@ -1,27 +1,24 @@
 from cs1graphics import *
 from random import *
 
+squareWidth = 100
+squareHeight = 100
+
 class Block(object):
     def __init__(self, value, column, row):
         self.value = value
         self.column = column
         self.row = row
-        self.__squareWidth= 100
-        self.__squareHeight= 100
         self.blockSquare, self.blockText = self.setDisplay()
 
     def setDisplay(self):
         column = self.column; row = self.row
-        x=(column*self.__squareWidth)+(self.__squareWidth//2)
-        y=(row*self.__squareHeight)+(self.__squareHeight//2)        
-        blockSquare= Square(min(self.__squareWidth*4//5,self.__squareHeight*4//5),Point(x,y))
+        x = (column * squareWidth) + (squareWidth // 2)
+        y = (row * squareHeight) + (squareHeight // 2)        
+        blockSquare = Square(min(squareWidth * 4 // 5, squareHeight * 4 // 5), Point(x, y))
         blockSquare.setFillColor("yellow")
-        blockText = Text(str(self.value), 30, Point(x,y))
+        blockText = Text(str(self.value), 30, Point(x, y))
         return blockSquare, blockText
-
-    def merge(self, block):
-        if self.value == block.value: return Block(self.value + block.value)
-        else: return None
         
 class Board(object):
     def __init__(self, numberOfColumns, numberOfRows):
@@ -53,50 +50,46 @@ class Game2048(object):
         self.__numberOfColumns = numberOfColumns
         self.__numberOfRows = numberOfRows
         self.gameBoard = Board(numberOfColumns, numberOfRows)
-        self.__squareWidth=canvas.getWidth()//numberOfColumns
-        self.__squareHeight=canvas.getHeight()//numberOfRows
 
         self.__blockLayer = Layer()
         self.__blockLayer.setDepth(0)
         canvas.add(self.__blockLayer)
 
-        self.__GridLayer= Layer()
+        self.__gridLayer = Layer()
         self.__displayGrid()
 
     def getBoard(self):
         return self.gameBoard
     
     def __displayGrid(self):
-        for column in range(0,self.__numberOfColumns+1):
-            p=Path(Point(self.__squareWidth*column,0),
-                   Point(self.__squareWidth*column,self.__canvas.getHeight()))
+        for column in range(self.__numberOfColumns + 1):
+            p = Path(Point(squareWidth * column, 0),
+                   Point(squareWidth * column, self.__canvas.getHeight()))
             p.setBorderColor('black')
             p.setBorderWidth(1)
-            self.__GridLayer.add(p)
+            self.__gridLayer.add(p)
         
-        for row in range(0,self.__numberOfRows+1):
-            p=Path(Point(0,self.__squareHeight*row),
-                   Point(self.__canvas.getWidth(),self.__squareHeight*row))
+        for row in range(self.__numberOfRows + 1):
+            p = Path(Point(0, squareHeight * row),
+                   Point(self.__canvas.getWidth(), squareHeight * row))
             p.setBorderColor('black')
             p.setBorderWidth(1)
-            self.__GridLayer.add(p)
-        self.__canvas.add(self.__GridLayer) 
+            self.__gridLayer.add(p)
+        self.__canvas.add(self.__gridLayer) 
 
     def displayBlocks(self):
         self.__blockLayer.clear()
-        for column in range(0,self.__numberOfColumns):
-            for row in range(0,self.__numberOfRows):
+        for column in range(self.__numberOfColumns):
+            for row in range(self.__numberOfRows):
                 blocks = self.getBoard().getBlocks()
                 if blocks[column][row] != None:
                     self.__blockLayer.add(blocks[column][row].blockSquare)
                     self.__blockLayer.add(blocks[column][row].blockText)
 
-        
-
 def runGame():
     NumberOfColumns = 6
     NumberOfRows = 6
-    canvas = Canvas(100*NumberOfColumns , 100*NumberOfRows,'white','2048')
+    canvas = Canvas(squareHeight * NumberOfColumns, squareWidth * NumberOfRows, 'white', '2048')
     
     G2048 = Game2048(canvas, NumberOfColumns, NumberOfRows)
     G2048Board = G2048.getBoard()
