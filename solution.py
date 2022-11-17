44bea07839b9265f7f4087dfefbe32234c32f6e9
flakepowders <42159553+flakepowders@users.noreply.github.com>
2022-11-17 17:56:36 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,9 +2,22 @@ from cs1graphics import *
 from random import *
 
 class Block(object):
-    def __init__(self, value, text):
+    def __init__(self, value, column, row):
         self.value = value
-        self.text = text # Canvas 상에 표시될 text
+        self.column = column
+        self.row = row
+        self.__squareWidth= 100
+        self.__squareHeight= 100
+        self.blockSquare, self.blockText = self.setDisplay()
+
+    def setDisplay(self):
+        column = self.column; row = self.row
+        x=(column*self.__squareWidth)+(self.__squareWidth//2)
+        y=(row*self.__squareHeight)+(self.__squareHeight//2)        
+        blockSquare= Square(min(self.__squareWidth*4//5,self.__squareHeight*4//5),Point(x,y))
+        blockSquare.setFillColor("yellow")
+        blockText = Text(str(self.value), 30, Point(x,y))
+        return blockSquare, blockText
 
     def merge(self, block):
         if self.value == block.value: return Block(self.value + block.value)
@@ -22,11 +35,11 @@ class Board(object):
     def setBlocks(self, blocks):
         self.blocks = blocks
     
-    def setBlock(self, coord, block):
-        self.blocks[coord[0]][coord[1]] = block
-
-    def getFree(self):
-        pass # 모든 빈칸의 위치를 반환
+    def setBlock(self, block):
+        if block.value != 0:
+            self.blocks[block.column][block.row] = block
+        else:
+            self.blocks[block.column][block.row] = None
 
     def addBlock(self, coord):
         pass # 빈칸 중 1개의 랜덤한 위치에 새 블록 하나 추가
@@ -35,18 +48,23 @@ class Board(object):
         pass # 플레이어의 조작, 블록들이 움직이고 충돌하는 것 처리
 
 class Game2048(object):
-    def __init__(self, canvas, numberOfColumns,numberOfRows):
+    def __init__(self, canvas, numberOfColumns, numberOfRows):
         self.__canvas = canvas
-        self.__L2048 = Layer()
         self.__numberOfColumns = numberOfColumns
-        self.__numberOfRows = numberOfRows          
+        self.__numberOfRows = numberOfRows
+        self.gameBoard = Board(numberOfColumns, numberOfRows)
         self.__squareWidth=canvas.getWidth()//numberOfColumns
         self.__squareHeight=canvas.getHeight()//numberOfRows
-        self.__L2048.setDepth(0)
-        canvas.add(self.__L2048)
+
+        self.__blockLayer = Layer()
+        self.__blockLayer.setDepth(0)
+        canvas.add(self.__blockLayer)
 
         self.__GridLayer= Layer()
         self.__displayGrid()
+
+    def getBoard(self):
+        return self.gameBoard
     
     def __displayGrid(self):
         for column in range(0,self.__numberOfColumns+1):
@@ -64,11 +82,32 @@ class Game2048(object):
             self.__GridLayer.add(p)
         self.__canvas.add(self.__GridLayer) 
 
+    def displayBlocks(self):
+        self.__blockLayer.clear()
+        for column in range(0,self.__numberOfColumns):
+            for row in range(0,self.__numberOfRows):
+                blocks = self.getBoard().getBlocks()
+                if blocks[column][row] != None:
+                    self.__blockLayer.add(blocks[column][row].blockSquare)
+                    self.__blockLayer.add(blocks[column][row].blockText)
+
+        
+
 def runGame():
     NumberOfColumns = 6
     NumberOfRows = 6
     canvas = Canvas(100*NumberOfColumns , 100*NumberOfRows,'white','2048')
     
     G2048 = Game2048(canvas, NumberOfColumns, NumberOfRows)
+    G2048Board = G2048.getBoard()
+    G2048Board.setBlock(Block(2, 1, 1))
+    G2048.displayBlocks()
+    while True:
+        e = canvas.wait()
+        key = e.getKey()
+        print(key)
+        if key == "w":
+            G2048Board.move((-1, 0))
+        G2048.displayBlocks()
 
 runGame()
