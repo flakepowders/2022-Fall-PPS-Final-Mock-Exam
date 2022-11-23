5b4a6d3bf7321976c5bc310b874fbaeb9d16523b
flakepowders <42159553+flakepowders@users.noreply.github.com>
2022-11-24 00:51:51 +0900

    Update solution.py
    added game over message

--- solution.py
+++ solution.py
@@ -8,17 +8,17 @@ class Board(object):
     def __init__(self, numberOfColumns, numberOfRows):
         self.__numberOfColumns = numberOfColumns
         self.__numberOfRows = numberOfRows
-        self.blocks = [[None] * numberOfRows for i in range(numberOfColumns)]
-        self.newBlockCoord = None
+        self.__blocks = [[None] * numberOfRows for i in range(numberOfColumns)]
+        self.__newBlockCoord = None
         
     def getBlock(self, column, row):
-        return self.blocks[column][row]
+        return self.__blocks[column][row]
     
     def setBlock(self, block, column, row):
-        self.blocks[column][row] = block
+        self.__blocks[column][row] = block
 
     def getNewBlockCoord(self):
-        return self.newBlockCoord
+        return self.__newBlockCoord
 
     def addBlock(self):
         # https://github.com/gabrielecirulli/2048/blob/master/js/game_manager.js
@@ -26,12 +26,17 @@ class Board(object):
         empty = []
         for i in range(self.__numberOfColumns):
             for j in range(self.__numberOfRows):
-                if self.blocks[i][j] is None:
+                if self.__blocks[i][j] is None:
                     empty.append((i, j))
+
+        if len(empty) == 0:
+            return True
+
         c = choice(empty)
         block = 2 if randrange(10)<9 else 4
-        self.newBlockCoord = (c[0], c[1])
-        self.blocks[c[0]][c[1]] = block
+        self.__newBlockCoord = (c[0], c[1])
+        self.__blocks[c[0]][c[1]] = block
+        return False
 
     def move(self, direction):
         # 블록들을 합침
@@ -50,10 +55,10 @@ class Board(object):
             return answer
 
         def collapseCoords(coords):
-            blocks = [self.blocks[c[0]][c[1]] for c in coords]
+            blocks = [self.__blocks[c[0]][c[1]] for c in coords]
             newBlocks = collapse(blocks)
             for i, c in enumerate(coords):
-                self.blocks[c[0]][c[1]] = newBlocks[i]
+                self.__blocks[c[0]][c[1]] = newBlocks[i]
 
         if direction == "w":
             for i in range(self.__numberOfColumns):
@@ -73,19 +78,20 @@ class Game2048(object):
         self.__canvas = canvas
         self.__numberOfColumns = numberOfColumns
         self.__numberOfRows = numberOfRows
-        self.gameBoard = Board(numberOfColumns, numberOfRows)
+        self.__gameBoard = Board(numberOfColumns, numberOfRows)
 
         self.__blockLayer = Layer()
         self.__blockLayer.setDepth(0)
         canvas.add(self.__blockLayer)
 
-        self.__gridLayer = Layer()
         self.__displayGrid()
 
     def getBoard(self):
-        return self.gameBoard
+        return self.__gameBoard
     
     def __displayGrid(self):
+        self.__gridLayer = Layer()
+
         for column in range(self.__numberOfColumns + 1):
             p = Path(Point(squareWidth * column, 0),
                    Point(squareWidth * column, self.__canvas.getHeight()))
@@ -109,8 +115,7 @@ class Game2048(object):
                 if block != None:
                     x = (column * squareWidth) + (squareWidth // 2)
                     y = (row * squareHeight) + (squareHeight // 2)
-                    blockSquare = Square(min(squareWidth*4//5, squareHeight*4//5),Point(x,y))
-                    blockSquare = Square(min(squareWidth * 4 // 5, squareHeight * 4 // 5), Point(x, y))
+                    blockSquare = Square(min(squareWidth * 4 // 5, squareHeight * 4 // 5), Point(x,y))
                     if (column, row) != self.getBoard().getNewBlockCoord():
                         blockSquare.setFillColor("yellow")
                     else: blockSquare.setFillColor("orange")
@@ -118,21 +123,30 @@ class Game2048(object):
                     self.__blockLayer.add(blockSquare)
                     self.__blockLayer.add(blockText)
 
+    def showGameOverMessage(self):
+        self.__messageLayer = Layer()
+        self.__messageLayer.setDepth(-100)
+        gameOverMessage = Text("Game Over!", 20, Point(self.__canvas.getWidth() // 2, self.__canvas.getHeight() // 2))
+        self.__messageLayer.add(gameOverMessage)
+        self.__canvas.add(self.__messageLayer) 
+
 def runGame():
-    NumberOfColumns = 6
-    NumberOfRows = 6
+    NumberOfColumns = 3
+    NumberOfRows = 3
+
     canvas = Canvas(squareHeight * NumberOfColumns, squareWidth * NumberOfRows, 'white', '2048')
-    
     G2048 = Game2048(canvas, NumberOfColumns, NumberOfRows)
     G2048Board = G2048.getBoard()
+
     G2048Board.addBlock()
     G2048.displayBlocks()
     while True:
         e = canvas.wait()
         key = e.getKey()
-        if key in ["w", "a", "s", "d"]:
+        if key in ["w", "s", "a", "d"]:
             G2048Board.move(key)
-            G2048Board.addBlock()
+            gameOver = G2048Board.addBlock()
+            if gameOver: break
             G2048.displayBlocks()
-
+    G2048.showGameOverMessage()
 runGame()
